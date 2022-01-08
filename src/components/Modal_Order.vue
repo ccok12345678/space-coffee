bbb4a05e59ad1d04abab028ef430061fcc50e2ed
Karl Yang <ccok12345678@gmail.com>
2022-01-08 16:47:36 +0800

    added order modal

---
+++ src/components/Modal_Order.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+#modal_order.modal.fade(tabindex="-1" ref="modal")
+  .modal-dialog.modal-lg.modal-dialog-centered
+    .modal-content.p-3()
+      .modal-header.p-2.mb-3
+      p {{ tempOrder }}
+</template>
+
+<script>
+import modalMixin from '@/mixins/modalMixin';
+
+export default {
+  data() {
+    return {
+      tempOrder: {},
+    };
+  },
+  props: {
+    order: {
+      type: Object,
+      default() { return {}; },
+    },
+  },
+  mixins: [modalMixin],
+  watch: {
+    order() {
+      this.tempOrder = this.order;
+      console.log('order modal', this.tempOrder);
+    },
+  },
+};
+</script>
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -6,6 +6,7 @@ section.w-100.overflow-auto.text-nowrap(v-if="!!orders.length")
         th 下單時間
         th 買家
         th.text-center.d-none.d-sm-table-cell 訂單內容
+          i.bi.bi-pencil-square.ms-2
         th.text-center.d-table-cell.d-sm-none
           | 訂單編號
           i.bi.bi-pencil-square.ms-2
@@ -13,39 +14,45 @@ section.w-100.overflow-auto.text-nowrap(v-if="!!orders.length")
         th.text-center 付款
         th
     tbody
-      tr(v-for="order in orders" :key="order.id"
-        @click.prevent="")
+      tr(v-for="order in orders" :key="order.id")
         td {{ $filters.date(order.create_at) }}
         td {{ order.user.name }}
         td.d-none.d-sm-table-cell
-          p.m-0(v-for="(item, i) in order.products" :key="i")
-            | • {{ item.product.title }}（{{ item.product.unit }}）*{{ item.product.num }}
+          a.link-dark.text-decoration-none(href="#" @click.prevent="openModal(order)")
+            p.m-0(v-for="(item, i) in order.products" :key="i")
+              | • {{ item.product.title }}（{{ item.product.unit }}）*{{ item.product.num }}
         td.d-table-cell.d-sm-none
-          a.link-dark.text-decoration-none(href="#")
-            |{{ order.id }}
+          a.link-dark.text-decoration-none(href="#" @click.prevent="openModal(order)")
+            | • {{ order.id }}
         td {{ $filters.currency(order.total) }}
         td
           span.text-success(v-if="order.is_paid") 已付款
           span.text-danger(v-else) 未付款
         td.text-end
-          button.border-0.hover-gray.py-2.d-block.w-100(type="button" title="檢視編輯")
+          button.border-0.hover-gray.p-2.d-block.w-100(type="button" title="檢視編輯"
+            @click.prevent="openModal(order)")
             i.bi.bi-pencil-square
-          button.border-0.hover-red.py-2.d-block.w-100(type="button" title="刪除") X
+          button.border-0.hover-red.p-2.d-block.w-100(type="button" title="刪除")
+            i.bi.bi-trash
 Pagination(:pages="pagination" @emit-page="getOrders")
+OrderModal(ref="orderModal" :order="tempOrder")
 </template>
 
 <script>
 import Pagination from '@/components/Pagination.vue';
+import OrderModal from '@/components/Modal_Order.vue';
 
 export default {
   data() {
     return {
       orders: [],
+      tempOrder: {},
       pagination: {},
     };
   },
   components: {
     Pagination,
+    OrderModal,
   },
   inject: ['tokenValue'],
   methods: {
@@ -58,6 +65,10 @@ export default {
       this.orders = data.orders;
       this.pagination = data.pagination;
       console.log('orders', data);
+    },
+    openModal(order) {
+      this.tempOrder = { ...order };
+      this.$refs.orderModal.showModal();
     },
   },
   created() {
