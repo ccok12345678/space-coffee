502bed5535697606f26aa4bfa9d02e741fe6bfe4
Karl Yang <ccok12345678@gmail.com>
2022-03-02 21:36:21 +0800

    modified: changed toggler to dropdown

--- src/components/frontend/UserNavbar.vue
+++ src/components/frontend/UserNavbar.vue
@@ -23,29 +23,37 @@ header.sticky-mobile
 
           hr.me-5.my-0.d-md-none
 
-          #navAccordion.accordion.accordion-flush.d-flex.flex-column.flex-md-row
+          .d-flex.flex-column.flex-md-row
 
-            .accordion-item.border-0.mx-2
+            .dropdown.border-0.mx-2
 
-              a#area.nav-link.link-gray-500.accordion-button.py-2.hover-text-gray.collapsed(
-                href="#coffeeArea" data-bs-toggle="collapse" role="button")
+              a#area.nav-link.link-gray-500.dropdown-toggle.py-2.hover-text-gray(
+                href="#coffeeArea"
+                data-bs-toggle="dropdown"
+                role="button"
+                aria-expanded="false")
                 | 咖啡產區
-              #coffeeArea.collapse.vstack(data-bs-parent="#navAccordion")
-                router-link.link-gray-500.text-decoration-none.ps-2.py-1.hover-text-gray(
-                  :to="`/shop/${area}`" v-for="(area, key) in areas" :key="key")
-                  | {{ area }}
+              ul.dropdown-menu.border-3(aria-labelledby="area")
+                li(v-for="(area, key) in areas" :key="key")
+                  router-link.dropdown-item.link-gray-600.ps-2.py-1.hover-text-gray(
+                    :to="`/shop/${area}`" )
+                    | {{ area }}
 
             hr.me-5.my-0.d-md-none
 
-            .accordion-item.mx-2
-              a#roast.nav-link.link-gray-500.accordion-button.collapsed.py-2.hover-text-gray(
-                href="#coffeeRoast" data-bs-toggle="collapse" role="button")
+            .dropdown.mx-2
+              a#roast.nav-link.link-gray-500.dropdown-toggle.py-2.hover-text-gray(
+                href="#coffeeRoast"
+                data-bs-toggle="dropdown"
+                role="button"
+                aria-expanded="false")
                 | 咖啡焙度
 
-              #coffeeRoast.collapse.vstack(data-bs-parent="#navAccordion")
-                router-link.link-gray-500.text-decoration-none.ps-2.py-1.hover-text-gray(
-                  :to="`/shop/${roast}`" v-for="(roast, key) in menu.roasts" :key="key")
-                  | {{ roast }}
+              ul.dropdown-menu.border-3(aria-labelledby="roast")
+                li(v-for="(roast, key) in menu.roasts" :key="key")
+                  router-link.dropdown-item.link-gray-600.ps-2.py-1.hover-text-gray(
+                    :to="`/shop/${roast}`")
+                    | {{ roast }}
 
           hr.me-5.my-0.d-md-none
 
@@ -109,11 +117,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navbar-toggler:focus {
-  box-shadow: none;
-}
 .navbar {
   z-index: 100;
+}
+.navbar-toggler:focus {
+  box-shadow: none;
 }
 .nav-button::after {
   content: "";
@@ -124,5 +132,29 @@ export default {
   background-image: url("../../assets/images/telescope_icon.svg");
   background-repeat: no-repeat;
   background-size: 1.25rem;
+}
+.dropdown-toggle {
+  display: flex;
+
+  &::after {
+    content: "";
+    border: 0;
+    flex-shrink: 0;
+    width: 1.25rem;
+    height: 1.25rem;
+    margin-left: auto;
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 32 32' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' enable-background='new 0 0 32 32'%3e%3ccircle cx='16' cy='16' r='10' fill='none' stroke='%23adb5bd' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='10' class='stroke-000000'%3e%3c/circle%3e%3cpath d='M21.5 7.7c3.7-1.3 6.6-1.3 7.5.1 1.5 2.3-3 7.9-10.2 12.5s-14.1 6.3-15.6 4c-.9-1.4.3-3.8 2.8-6.5' fill='none' stroke='%23adb5bd' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='10' class='stroke-000000'%3e%3c/path%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-size: 1.25rem;
+    transition: transform 0.2s ease-in-out;
+  }
+  &:hover::after {
+    transform: rotate(-180deg);
+  }
+}
+.dropdown-menu {
+  @media (max-width: 767.98px) {
+    border: none;
+  }
 }
 </style>
